019a5d685858f8b2f27304515f61342f5450a824
Ariel Fogel <fogeltine@gmail.com>
2023-08-17 18:34:47 +0300

    add small helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,17 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def turbo_id_for(obj)
+    obj.persisted? ? obj.id : obj.hash
+  end
+
+  def sort_link_to(name, column, **options)
+    if params[:sort] == column.to_s
+      direction = params[:direction] == "asc" ? "desc" : "asc"
+    else
+      direction = "asc"
+    end
+
+    link_to name, request.params.merge(sort: column, direction: direction), **options
+  end
 end
